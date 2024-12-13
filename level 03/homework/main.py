6db842640712c872cf9e5c349b5cd914a5ca4ecd
Matejanezashvili <matejan0615@gmail.com>
2024-12-13 20:58:22 +0400

    Second commit

---
+++ level 03/homework/main.py
@@ -0,0 +1,78 @@
+print("mate janezashvili" * 100)
+# city => "London':
+# print"city)
+
+city = "London"
+print(city)
+# საშუალო არითმეკულის პოვნა:
+# რიცხვების ჯამი უნდა გავყოთ, მათ რაოდენობაზე
+
+age1 = 10
+age2 = 20
+age3 = 38
+
+sum = age1 + age2 + age3
+
+print(sum / 3)
+
+num1 = 2
+num2 = 3
+
+print(num1 * num1 + num2 * num2) # 13
+
+
+number1 = 10
+number2 = 5
+
+print(number1 + number2) # 15
+print(number1 - number2) # 5
+print(number1 * number2) # 50
+print(number1 / number2) # 2.0
+
+# კომენტარი გვეხმარება მარტივად ავღწეროთ ჩვენს მიერ დაწერილი კოდი, რაც მომავალში ამარტივებს მის გააზრებას.
+# კომპიუტერი კოდის კითხვისას აიგნორებს კომენტარებს(არ აგცევს ყურადღებას)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ level 04/homework/main.py
@@ -0,0 +1,48 @@
+name = "mate"
+name = "saba"
+name = "tamta"
+print(name)
+
+# snake_case არის ცვლადის შექმნის სტილი, როდესაც რამოდენიმე სტყვისგან შემდგარი ცვლადის შექმნისას, სიტყვებს შორის იწერება "_"
+my_car = "bmw"
+my_favorite_pet = "Dog"
+dog_name = "Bob"
+
+num1 = 13
+num2 = 11
+
+result = num1 * num2
+
+print(result)
+
+number1 = 20
+number2 = 10
+
+print(number1 + number2) # 30
+print(number1 - number2) # 10
+print(number1 * number2) # 200
+print(number1 / number2) # 2.0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
