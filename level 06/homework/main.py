c806c1cb591253bcaca4d23631a338854eabac90
Matejanezashvili <matejan0615@gmail.com>
2024-12-16 22:44:22 +0400

    Third commit

---
+++ level 06/homework/main.py
@@ -0,0 +1,26 @@
+# 1)
+
+sport = input("Enter your fav sport: ")
+
+print("Your Fav Sport is:", sport)
+
+# 2)
+
+name = input("Enter Your Name: ")
+lastname = input("Enter Your lastname: ")
+age = input("Enter Your Age: ")
+home = input("Enter Your home adress: ")
+city = input("Enter Your city: ")
+hobby = input("Enter Your hobby: ")
+
+print("Name:", name, "lastname:", lastname, "age:", age, "home adress:", home, "city:", city, "hobby:", hobby)
+
+# 3)
+
+num1 = int(input("Enter your num1: "))
+num2 = int(input("Enter your num2: "))
+
+print("+", num1 + num2)
+print("-", num1 - num2)
+print("*", num1 * num2)
+print("/", num1 / num2)
