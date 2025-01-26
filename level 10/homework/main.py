d88b8ae6b241c9d2a8e8cadecd2d6103d01eb058
Matejanezashvili <matejan0615@gmail.com>
2025-01-26 14:59:09 +0400

    commit

---
+++ level 10/homework/main.py
@@ -0,0 +1,19 @@
+x = 7
+x1 = 5
+print(x>x1)
+x2 = 5
+x3 = 7
+print(x2<x3)
+x4 = 8
+x5 = 20
+print(x4 <= x5)
+x6 = 20
+x7 = 31
+print(x6>x7)
+x8 = 60
+x9 = 40
+print(x8==x9)
+
+
+
+
---
+++ level 12/classwork/main.py
@@ -0,0 +1,10 @@
+print(3>1)# true
+print(3>10)# false
+print(30>3)# true
+print(3>2354)# false
+print(342<3452)# true
+print(3234>7899)# false
+print(18>14)# true
+print(12<8)# false
+print(323>222)# true
+print(32<28)# false
