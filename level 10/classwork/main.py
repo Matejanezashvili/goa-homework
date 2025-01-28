ef8f076cebcc4526c2a0c86ebf986fc63af74496
Matejanezashvili <matejan0615@gmail.com>
2025-01-28 18:26:12 +0400

    commit

---
+++ level 10/classwork/main.py
@@ -0,0 +1,10 @@
+print(90<900)# true
+print(34<101)# true
+print(3<4)# true
+print(25<=25)# true
+print(85>56)# true
+print(24>25)# false
+print(4<3)# false
+print(65==60)# false
+print(56>1000)# false
+print(100>=101)# false
---
+++ level 10/homework/main3.py
@@ -0,0 +1,3 @@
+num=int(input("enter number: "))
+num2=int(input("enter number: "))
+print(num>num2)
