e8a245169d5e98b808694f6e29b473b7b0454e4b
Matejanezashvili <matejan0615@gmail.com>
2025-04-27 21:23:43 +0400

    commit

---
+++ level 28/homework/homework.py
@@ -0,0 +1,21 @@
+user_name=input("Whats your name: ")
+num=5
+print(user_name.lower())
+print(user_name.upper())
+print(user_name.capitalize())
+
+sentence=input("Type anything:")
+sentence2=input("Type anything:")
+sentence3=input("Type anything:")
+print(sentence.upper())
+print(sentence2.capitalize())
+print(sentence3.lower())
+
+name="Mate"
+others_name=input("Whats your name?")
+if name == others_name.capitalize():
+    print("We have similiar name")
+else: print("We have different names")
+
+string="goa is good"
+print(string.capitalize())
