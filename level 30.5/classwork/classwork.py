4a43fdb492932f59cf17a6051013a874b7aad7ee
Matejanezashvili <matejan0615@gmail.com>
2025-05-13 22:28:32 +0400

    commit

---
+++ level 30.5/classwork/classwork.py
@@ -0,0 +1,12 @@
+
+# ცვლადში შეინახეთ თქვენი სახელი, შემდეგ კი მომხმარებელსაც შემოატანინეთ თავისი სახელი, თუ თქვენი სახელები დაემთხვევა გამოუტანეთ "Our names are
+# similar !", სხვა შემთხვევაში - "We have different names". hint: დაგჭირდებათ პირობითი განცხადებები და სტრინგის მეთოდები. ეს შემოწმება უნდა იყოს case-insensitive
+
+my_name = "mate"
+user_name = input("Enter your name: ")
+
+
+if my_name.lower() == user_name.lower():
+    print("Our names are similar!")
+else:
+    print("We have different names.")
--- level 30/classwork/classwork.py
+++ level 30/classwork/classwork.py
@@ -8,3 +8,5 @@ fruits = ["Melon", "Orange", "Banana", "Watermelon", "Kiwi"]
 fruits.pop(4)
 fruits.insert(3,"Pomegranate")
 print(fruits)
+
+# Upper- uppercase-ში გადაიყვანს სტრინგს lower ლოვექეისში Capitalize- პირველი ასო აფეთქებაშია დანარჩენი კი ლოვერში
